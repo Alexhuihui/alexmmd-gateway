1a216fefe0aa898ee64f9bdfa168532022692068
alex <2930807240@qq.com>
2021-12-27 22:55:31 +0800

    搭建好了框架

---
+++ src/main/java/top/alexmmd/gateway/AlexmmdGatewayApplication.java
@@ -0,0 +1,13 @@
+package top.alexmmd.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexmmdGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexmmdGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/alexmmd/gateway/AlexmmdGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package top.alexmmd.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexmmdGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
